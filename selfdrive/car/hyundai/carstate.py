e7af517b847e83bdb65d692251268ee9cf213f59
zulufoxtrot <youri.lcd@gmail.com>
2024-08-17 10:40:00 +0200

    fix speed limiter: attempt #2bis

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -191,7 +191,7 @@ class CarState(CarStateBase):
     self.prev_main_buttons = self.main_buttons[-1]
     self.main_buttons.extend(cp.vl_all["CLU11"]["CF_Clu_CruiseSwMain"])
     if self.prev_main_buttons == 0 and self.main_buttons[-1] != 0:
-      self.main_enabled = not self.main_enabled
+      self.main_enabled = not self.main_enabled and not cp.vl["E_EMS11"]["Cruise_Limit_Status"] == 1
 
     # FrogPilot CarState functions
     fp_ret.brakeLights = bool(cp.vl["TCS13"]["BrakeLight"])
